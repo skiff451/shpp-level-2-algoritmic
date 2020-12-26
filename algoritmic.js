a86a436759250dc13d40ffcbbc0f82826ebe5a53
Serghey <samsomskiff451@gmail.com>
2020-12-27 01:19:19 +0200

    finish algoritmic

--- algoritmic.js
+++ algoritmic.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const cities = 
-`15,20,Кропивницкий,200000
+const cities =
+    `15,20,Кропивницкий,200000
 10,25,Киев,200000000
 10,20,Харьков,90000000
 
@@ -10,19 +10,19 @@ const cities =
 19,34,Мариуполь,5000000
 37,89,Львов,4000000
 12,98,Полтава,47000000
+
 23,13,Запорожье,5700000
 78,34,Одесса,67000000
 07,45,Ужгород,37000000
 09,12,Луцк,9900000
 99,11,Тернополь,1500000
-
 `
-const otherFunc = parseCSV(cities);
 
-otherFunc();
+// const otherFunc = parseCSV(cities);
+console.log(parseCSV(cities)('Харьков'));
+
 
 function parseCSV(cities) {
-
     const citiesInformation = cities.split('\n')
         .filter((cityData) => {
             if (cityData[0] !== '#' && cityData.length !== 0) {
@@ -44,15 +44,24 @@ function parseCSV(cities) {
         })
         .sort((a, b) => (b.population - a.population))
         .slice(0, 10)
-        .reduce((cities, currentCity) => {
+        .reduce((cities, currentCity, index) => {
             const cityName = currentCity.name;
-            cities[cityName] = currentCity;
+            cities[cityName] = {
+                population: currentCity.population,
+                rating: index + 1
+            };
             return cities;
         }, {})
 
-     
 
     return (someText) => {
-        console.log('obj-other', citiesInformation);
+        const citiesNames = Object.keys(citiesInformation);
+        let answer = 'there is no such city in the base, try more';
+        citiesNames.forEach(item => {
+            if (item === someText) {
+                answer = `${item} (${citiesInformation[item].rating} место в ТОП-10 самых крупных городов Украины, население ${citiesInformation[item].population} человек)`;
+            }
+        })
+        return answer;
     }
 }
